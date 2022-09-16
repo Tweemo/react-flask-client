9374a37990f4d11a565b3c149cc7fe9670b28d45
Tweemo <timothyliew88@hotmail.com>
2022-09-16 12:28:56 +1200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+
+  const [data, setData] = useState({"members": []});
+
+  useEffect(() => { 
+    fetch("/members").then (
+      res => res.json()
+    ).then(
+      data => {
+      setData(data)
+      console.log(data)
+      }
+    )
+  }, [])
+console.log(data.members)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      {typeof data.members === 'undefined' ? (
+        <p>loading...</p>
+      ) : (
+        data.members.map((member: any, i: number) => (
+          <p key={i}>{member}</p>
+        ))
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
